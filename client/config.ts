4c5ff027b59631f0a22609a928684c05cb123c4e
Mr Vic Valenti <110475556+MrValenti@users.noreply.github.com>
2024-11-18 16:16:45 +0200

    Update config.ts
    change to production

--- client/config.ts
+++ client/config.ts
@@ -6,7 +6,7 @@ interface Config {
     switch (server) {
       case "production":
         config = {
-          baseUrl: "",
+          baseUrl: "https://glo-collections.vercel.app/",
         };
         break;
       case "local":
@@ -20,6 +20,6 @@ interface Config {
     return config;
   };
   
-  export const selectServer = "local";
+  export const selectServer = "production";
   export const config = checkConfig(selectServer) as Config;
   
