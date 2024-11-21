ea207b43a108c8e36b817a6b2c0dbc326181d8e8
Mr Vic Valenti <110475556+MrValenti@users.noreply.github.com>
2024-11-21 16:59:33 +0200

    Add files via upload

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import BannerCategories from "./ui/BannerCategories"
+// import BannerCategories from "./ui/BannerCategories"
 // import Container from "./ui/Container"
 // import Footer from "./ui/Footer"
 // import Header from "./ui/Header"
@@ -6,16 +6,21 @@ import 'react-multi-carousel/lib/styles.css';
 import HomeBanner from "./ui/HomeBanner";
 import Highlights from "./ui/Highlights";
 import Categories from "./ui/Categories";
+import ProductListHome from './ui/ProductListHome';
 
 function App() {
 
   return (
     <main>
-      <BannerCategories />
       <HomeBanner />
+      <ProductListHome />
       <Highlights />
       <Categories />
-      {/* <Container>
+      
+      {/*<BannerCategories />
+      
+      
+       <Container>
         <p>Ecommerce Demo</p>
       </Container> */}
     </main>
