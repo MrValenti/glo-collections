2f2f9d6e2df6911770e4e50f23b15856b4a759e7
Mr Vic Valenti <110475556+MrValenti@users.noreply.github.com>
2024-12-10 16:27:24 +0200

    10-12-24 Files

--- client/src/ui/Footer.tsx
+++ client/src/ui/Footer.tsx
@@ -1,5 +1,5 @@
 import Container from "./Container";
-import { payment } from "../assets";
+// import { payment } from "../assets";
 import FooterTop from './FooterTop';
 import { Link } from "react-router-dom";
 
@@ -8,14 +8,17 @@ const Footer = () => {
     <div className="mt-10 bg-darkText text-whiteText">
       <FooterTop />
       <div className="text-center text-sm">
-        <Link to={"/"}>Terms of Use</Link> |
-        <Link to={"/"}> Privacy Policy</Link> |
-        <Link to={"/"}> Imprints</Link>
-      </div>
-      <Container className="flex flex-col md:flex-row items-center gap-4 justify-between">
-        
+        <Link to={"/"} style={{ fontSize: "18px" }}>Terms of Use</Link> |
+        <Link to={"/"} style={{ fontSize: "18px" }}> Privacy Policy</Link> |
+        <Link to={"/"} style={{ fontSize: "18px" }}> Imprints</Link>
+        <div></div>
         <p>Copyright © 2024 glocollections.co.za. All rights reserved.</p>
-        <img src={payment} alt="payment-img" className="object-cover" />
+      </div>
+      
+      <Container className="flex flex-col md:flex-row text-center gap-4 justify-between">
+        
+        {/* <p>Copyright © 2024 glocollections.co.za. All rights reserved.</p> */}
+        {/* <img src={payment} alt="payment-img" className="object-cover" /> */}
       </Container>
     </div>
   )
