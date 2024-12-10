e458b98caf5b345be394e02d179163ccd62917df
Mr Vic Valenti <110475556+MrValenti@users.noreply.github.com>
2024-12-10 16:51:46 +0200

    Update Footer.tsx

--- client/src/ui/Footer.tsx
+++ client/src/ui/Footer.tsx
@@ -1,4 +1,4 @@
-import Container from "./Container";
+//import Container from "./Container";
 // import { payment } from "../assets";
 import FooterTop from './FooterTop';
 import { Link } from "react-router-dom";
@@ -15,11 +15,11 @@ const Footer = () => {
         <p>Copyright © 2024 glocollections.co.za. All rights reserved.</p>
       </div>
       
-      <Container className="flex flex-col md:flex-row text-center gap-4 justify-between">
+      {/* <Container className="flex flex-col md:flex-row text-center gap-4 justify-between">
         
-        {/* <p>Copyright © 2024 glocollections.co.za. All rights reserved.</p> */}
-        {/* <img src={payment} alt="payment-img" className="object-cover" /> */}
-      </Container>
+         <p>Copyright © 2024 glocollections.co.za. All rights reserved.</p> 
+         <img src={payment} alt="payment-img" className="object-cover" /> 
+      </Container> */}
     </div>
   )
 }
